563613cfac4a98987454517b2cd4a8b946a01426
Jooey Zhong <zhongjunyi.dev@gmail.com>
2017-11-06 21:49:14 +0100

    Create 二位数组索引.py

---
+++ 二位数组索引.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print([2][2])
